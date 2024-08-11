e6ad90b06dff55b50aedff6ac19df194d81704ce
DELL <omnianabil1920@gmail.com>
2024-08-11 14:53:50 +0300

    implemented the adapter

--- app/src/main/java/com/example/contactmanagerkotlin/MainActivity.kt
+++ app/src/main/java/com/example/contactmanagerkotlin/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.contactmanagerkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.contactmanagerkotlin.databinding.ActivityMainBinding
+import com.example.contactmanagerkotlin.repo.ContactRepository
+import com.example.contactmanagerkotlin.room.ContactDataBase
+import com.example.contactmanagerkotlin.viewmodel.ContactViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var contactViewModel: ContactViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val doa = ContactDataBase.getInstance(applicationContext).contactDAO
+        val repository = ContactRepository(doa)
+        contactViewModel = ViewModelProvider(
+            this,
+        ).get(ContactViewModel::class.java)
+        binding.contactViewModel = contactViewModel
+        binding.lifecycleOwner = this
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        displayUsersList()
+    }
+
+    private fun displayUsersList() {
+        contactViewModel.users.observe(this , Observer {
+            binding.recyclerView.adapter =
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/contactmanagerkotlin/view/MyRecyclerViewAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.contactmanagerkotlin.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contactmanagerkotlin.R
+import com.example.contactmanagerkotlin.databinding.CardItemBinding
+import com.example.contactmanagerkotlin.room.Contacts
+
+class MyRecyclerViewAdapter(
+    private val contactList: List<Contacts>,
+    private val clickListener: (Contacts) -> Unit
+) :
+    RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder>() {
+
+
+    class MyViewHolder(var binding: CardItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(contact : Contacts , clickListener: (Contacts) -> Unit){
+            binding.nameTextView.text = contact.contact_name
+            binding.emailTextView.text = contact.contact_email
+            binding.listItemLayout.setOnClickListener{
+                clickListener(contact)
+
+        }
+
+    }
+
+
+}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding : CardItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.card_item,
+            parent,
+            false
+        )
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return  contactList.size
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(contactList[position] , clickListener)
+
+    }
+}
