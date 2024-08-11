ff4725074af8d85c50e17191c7f170885aac6b3c
DELL <omnianabil1920@gmail.com>
2024-08-11 12:38:27 +0300

    implemented repository

---
+++ app/src/main/java/com/example/contactmanagerkotlin/repo/ContactRepository.kt
@@ -0,0 +1,20 @@
+package com.example.contactmanagerkotlin.repo
+
+import com.example.contactmanagerkotlin.room.ContactDAO
+import com.example.contactmanagerkotlin.room.Contacts
+
+class ContactRepository(private val contactDAO: ContactDAO) {
+    val contact = contactDAO.getAllContactsInDB()
+    suspend fun insert(contact: Contacts): Long {
+        return contactDAO.insertContact(contact)
+    }
+    suspend fun update(contact: Contacts)  {
+        return contactDAO.updateContact(contact)
+    }
+    suspend fun delete(contact: Contacts) {
+        return contactDAO.deleteContact(contact)
+    }
+    suspend fun deleteAll(contact: Contacts) {
+        return contactDAO.deleteAllContacts()
+    }
+}
